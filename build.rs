7eac2ea32367ffb10c2307a1c88ed10707c33b71
yihuishou <gp095@hotmail.com>
2026-05-17 15:50:44 +0800

    🏷️ 更新包名和 exe 文件名为 llama_cpp_lunch

--- build.rs
+++ build.rs
@@ -6,7 +6,7 @@ fn main() {
     res.set("FileDescription", "llama.cpp lunch - GUI Launcher");
     res.set("LegalCopyright", "Copyright 2025");
     res.set("InternalName", "llama.cpp lunch");
-    res.set("OriginalFilename", "llama-cpp-lunch.exe");
+    res.set("OriginalFilename", "llama_cpp_lunch.exe");
     res.set_version_info(winres::VersionInfo::FILEVERSION, 0x0000000100000000u64);
     res.set_version_info(winres::VersionInfo::PRODUCTVERSION, 0x0000000100000000u64);
     res.set_version_info(winres::VersionInfo::FILEOS, 0x40004u64);
