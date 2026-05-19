c20ca1f6bbb48ddc0c735db104863df1a21f9195
yihuishou <gp095@hotmail.com>
2026-05-19 16:37:34 +0800

    📝(i18n): tensor split label updated zh/en

--- src/ui/launch_commands_panel.rs
+++ src/ui/launch_commands_panel.rs
@@ -4,6 +4,7 @@ use crate::i18n;
 
 pub fn ui(ui: &mut egui::Ui, server: &ServerManager, rpc: &RpcManager, lang: &i18n::Language) {
     ui.heading(i18n::t(i18n::Key::SectionLaunchCommands, lang));
+    ui.separator();
     ui.add_space(4.0);
 
     // Server 启动命令
@@ -19,7 +20,8 @@ pub fn ui(ui: &mut egui::Ui, server: &ServerManager, rpc: &RpcManager, lang: &i1
     } else {
         ui.colored_label(egui::Color32::GRAY, i18n::t(i18n::Key::HintNoCommand, lang));
     }
-    ui.add_space(8.0);
+    ui.separator();
+    ui.add_space(4.0);
 
     // RPC 启动命令
     ui.label(i18n::t(i18n::Key::LabelRpcCommand, lang));
